04d3dbcf34218ae1259accf96fc1cf84003d4e75
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-05-15 11:26:44 -0300

    Delete B/Aula17-04 (Condicionais) directory

--- B/Aula17-04 (Condicionais)/desafio1.py
+++
@@ -1,9 +0,0 @@
-numero = int(input("==== Seja bem vindo à calculadora de palíndromo ====\n"
-             "Digite um número: "))
-
-numero = str(numero)
-
-if numero == numero[::-1]:
-    print(f"O número {numero} é um palíndromo!")
-else:
-    print(f"O número {numero} não é um palíndromo!")
