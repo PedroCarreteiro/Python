67cec09c3d6168ea4d53e4970aae17a239f36a1b
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-06-05 16:38:03 -0300

    Update enzo.py

--- Zap/enzo.py
+++ Zap/enzo.py
@@ -2,7 +2,7 @@ import pywhatkit as py
 import pyautogui
 import time
 
-py.sendwhatmsg_instantly("+55 19 99804-8226", "vc achou que iria sair impune!")
+py.sendwhatmsg_instantly("numero", "vc achou que iria sair impune!")
 while True:
     pyautogui.write("você achou que iria sair impune MAGU!")
     pyautogui.press("enter")
