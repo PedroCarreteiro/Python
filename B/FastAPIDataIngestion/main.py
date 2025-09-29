0dcbb52e982280df73f380e823a4e5fdec31caaa
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-09-29 10:27:29 -0300

    Correção

--- B/FastAPIDataIngestion/main.py
+++ B/FastAPIDataIngestion/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Turmas API com Banco")
 
 origins = ["http://localhost", "http://localhost:8080", "http://localhost:5500", "http://127.0.0.1:5500"]
 
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, all_methods=["*"], allow_headers=["*"]) 
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]) 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
